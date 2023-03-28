f84e6df8c06aae6926428f4fca63f4225aa48b41
Tayonna25 <113479181+Tayonna25@users.noreply.github.com>
2023-03-27 20:07:57 -0400

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Tayonna and MSU small change!
         </a>
       </header>
     </div>
