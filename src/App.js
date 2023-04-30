edacbd4f378a33188c7a8d316097975c2dcb4490
Tayonna25 <113479181+Tayonna25@users.noreply.github.com>
2023-04-30 10:12:17 -0400

    tt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import { supabase } from './supabaseClient';
 import React, { useState } from "react";
 import "./App.css";
+import {MagicButton} from "./MagicButton"
+
+
+function MagicButton () { 
+  const [count, setCount]= useState(0)
+  function doMagic() {
+    setCount(count + 1);
+  }
+  return (
+  <>
+   <h3>This is a magic button</h3>
+   <button onClick={doMagic}>Magic {count}</button>
+   </>
+
+
+
+  );
+}
+
+
 
 function App() {
   const [isGreen, setIsGreen] = useState(false);
